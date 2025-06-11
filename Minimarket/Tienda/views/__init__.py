7c12ae00fd6073c7e446a234842e452ba445250e
Massimo Larger <massimoalberto.larger@alumnos.ulagos.cl>
2025-06-11 15:38:07 -0400

    Ajustes finales

---
+++ Minimarket/Tienda/views/__init__.py
@@ -0,0 +1,7 @@
+from .auth_views import *
+from .product_views import *
+from .category_views import *
+from .sale_views import *
+from .offer_views import *
+from .inventory_views import *
+from .supplier_views import *
